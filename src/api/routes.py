e1cd9bd32be0ff6274148eab507b9191c11a7c42
Marco Gonzalo Gómez Pérez <410890+marcogonzalo@users.noreply.github.com>
2025-05-18 22:21:10 +0200

    Rename accruals router

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,7 +6,7 @@ from src.api.services.endpoints.service import router as service_router
 from src.api.services.endpoints.service_period import router as service_period_router
 from src.api.services.endpoints.service_contract import router as service_contract_router
 from src.api.accruals.endpoints.accrued_period import router as accrual_router
-from src.api.accruals.endpoints.period_processor import router as period_processor_router
+from src.api.accruals.endpoints.accruals import router as accruals_router
 from src.api.integrations.endpoints.holded import router as holded_router
 from src.api.integrations.endpoints.fourgeeks import router as fourgeeks_router
 
@@ -21,7 +21,7 @@ api_router.include_router(service_router)
 api_router.include_router(service_period_router)
 api_router.include_router(service_contract_router)
 api_router.include_router(accrual_router)
-api_router.include_router(period_processor_router)
+api_router.include_router(accruals_router)
 
 @api_router.route('/hello', methods=['POST', 'GET'])
 def handle_hello(request: Request):
